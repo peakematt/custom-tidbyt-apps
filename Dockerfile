377c4acec990d193c1432003f0064395350fb9e8
Matt Boehlke <matthew.boehlke@gmail.com>
2022-06-26 12:24:28 -0400

    Build container to push updates

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:20.04
+
+ENV PYTHONBUTTERED=1
+
+RUN \
+    echo "**** Get Pixlet ****" && \
+    curl https://github.com/tidbyt/pixlet/releases/download/v0.17.9/pixlet_0.17.9_darwin_amd64.tar.gz -o /tmp/pixlet.tar.gz && \
+    tar -xf /tmp/pixlet.tar.gz -C /usr/bin && \
+    chmod +x /usr/bin/pixlet && \
+    rm -rf /tmp/pixlet.tar.gz && \
+    echo "**** Install Packages ****" && \
+    apt-get update -y && apt-get install -y -qq \
+    cron \
+    python3 \
+    python3-pip \
+    && rm -rf /var/lib/apt/lists/* 
+
+COPY src/ /src
+
+COPY app/custom-cron /etc/cron.d/custom-cron
+
+COPY app/script.py /app/script.py
+
+RUN chmod 0744 /etc/cron.d/custom-cron && \
+    crontab /etc/cron.d/custom-cron
+
+CMD ["cron", "-f"]
---
+++ app/script.py
@@ -0,0 +1,51 @@
+import os
+import pathlib
+import subprocess
+
+
+def sub_variables(inline: str) -> str:
+    inline = inline.replace("ENVIRON_REPLACE_N8N_PASS", os.getenv("N8N_PASS"))
+
+    return inline
+
+
+def RunThis(cmd: list) -> None:
+    cmd = " ".join(cmd)
+    p = subprocess.Popen(cmd, shell=True)
+    p.wait()
+
+
+def main():
+    src_path = pathlib.Path("/src")
+    for file in src_path.rglob("*.star"):
+        infile = open(file)
+        outfile = open(f"/tmp/{file.name}")
+
+        for line in infile.readline():
+            outfile.write(sub_variables(line))
+
+        app_id = file.name.split("-")[0]
+
+        RunThis([
+            "pixlet",
+            "render",
+            f"{str(file)}",
+            "--output",
+            f"/tmp/{app_id}.webp"
+        ])
+
+        RunThis([
+            "pixlet",
+            "push",
+            "--api-token",
+            f"{os.getenv('TIDBYT_API_TOKEN')}",
+            f"{os.getenv('TIDBYT_DEVICE_ID')}",
+            "--installation-id",
+            f"{app_id}",
+            "--background",
+            f"/tmp/{app_id}.webp"
+        ])
+
+
+if __name__ == '__main__':
+    main()
